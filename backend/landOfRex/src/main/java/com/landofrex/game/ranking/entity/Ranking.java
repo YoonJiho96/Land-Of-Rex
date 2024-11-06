60869546797493a8227755fcd54959a6907faa76
박영훈 <pyh007264@gmail.com>
2024-11-07 08:52:46 +0900

    [BE] refactor: 사용하지 않는 import 삭제

--- backend/landOfRex/src/main/java/com/landofrex/game/ranking/entity/Ranking.java
+++ backend/landOfRex/src/main/java/com/landofrex/game/ranking/entity/Ranking.java
@@ -5,10 +5,6 @@ import com.landofrex.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-
-import java.math.BigInteger;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "ranking")
