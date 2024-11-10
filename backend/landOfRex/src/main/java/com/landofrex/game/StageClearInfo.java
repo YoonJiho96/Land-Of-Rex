bcf305614b158b07ce938669c7388801fc974fa0
박영훈 <pyh007264@gmail.com>
2024-11-11 08:58:04 +0900

    [BE] delete: 미사용 파일 삭제

--- backend/landOfRex/src/main/java/com/landofrex/game/StageClearInfo.java
+++
@@ -1,8 +0,0 @@
-package com.landofrex.game;//package com.infra.example.game;
-//
-//import java.time.LocalDateTime;
-//
-//public class StageClearInfo {
-//    private LocalDateTime clearedAt;
-//    private Integer goldAmount;
-//}
--- backend/landOfRex/src/main/java/com/landofrex/game/StageClearInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.landofrex.game;//package com.infra.example.game;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface StageClearInfoRepository extends JpaRepository<StageClearInfo,Long> {
-//}
