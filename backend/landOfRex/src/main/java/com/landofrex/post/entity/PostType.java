5425eac247d609e5ce49c8db7d54aa1d6ee51770
박영훈 <pyh007264@gmail.com>
2024-11-12 15:51:28 +0900

    [BE] feat: 문의사항 api 프론트와 연결

--- backend/landOfRex/src/main/java/com/landofrex/post/entity/PostType.java
+++ backend/landOfRex/src/main/java/com/landofrex/post/entity/PostType.java
@@ -6,10 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum PostType {
-    INQUIRY("문의사항", true),
-    QUESTION("질문", true),
-    FREE("자유게시글", false),
-    SUGGESTION("건의사항", true);
+    ACCOUNT_ISSUE("계정 문제", true),
+    GAME_FEEDBACK("게임 피드백", true),
+    BUG_REPORT("버그 신고", true),
+    SUGGESTION("건의 사항", true);
 
     private final String description;
     private final boolean inquiryType;  // 문의/답변이 필요한 타입인지
