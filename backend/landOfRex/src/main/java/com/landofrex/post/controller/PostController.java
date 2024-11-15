41b448fa918d08fc307c9b4d78e2cbe4b5f76a2a
박영훈 <pyh007264@gmail.com>
2024-11-15 17:05:41 +0900

    [BE] refactor: 필요없는 import 제거

--- backend/landOfRex/src/main/java/com/landofrex/post/controller/PostController.java
+++ backend/landOfRex/src/main/java/com/landofrex/post/controller/PostController.java
@@ -18,7 +18,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequiredArgsConstructor
