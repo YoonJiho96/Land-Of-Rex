3ad9761b3595028f78d3b20fcc1ae56d225a3905
정유진 <renascor96@naver.com>
2024-11-18 10:11:45 +0900

    [FE] [feat] 문의내역 상태 변경 api 연결

--- LandofRex/src/apis/apiSaveStatus.jsx
+++ LandofRex/src/apis/apiSaveStatus.jsx
@@ -1,22 +1,16 @@
 import axios from "axios";
 
 const apiSaveStatus = async (id, inquiryStatus) => {
-  const url = `https://k11e102.p.ssafy.io/api/v1/posts/${id}/inquiry-status`;
+  const url = `/api/v1/posts/${id}/inquiry-status`;
   const body = { inquiryStatus };
 
   try {
-    console.log("Sending PATCH request...");
-    console.log("URL:", url);
-    console.log("Body:", JSON.stringify(body));
-
     const response = await axios.patch(url, body, {
       withCredentials: true, // 쿠키 포함
     });
 
-    console.log("Response:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Error updating inquiry status:", error.response?.data || error.message);
     throw error;
   }
 };
